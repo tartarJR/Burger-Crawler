6d3a8ad13e2e6bbfe1477381d8a94986528a9a48
tartarJR <mustafaogun.ozturk@gmail.com>
2019-01-24 19:11:54 +0300

    added constants for parsing json

---
+++ src/main/java/com/tatar/burgercrawler/constant/JsonConstants.java
@@ -0,0 +1,14 @@
+package com.tatar.burgercrawler.constant;
+
+public class JsonConstants {
+
+    /* Keys used for parsing the API response */
+
+    public static final String KEY_RESPONSE = "response"; // JSON key name for the JSON object "response"
+    public static final String KEY_GROUPS = "groups"; // JSON key name for the JSON array "groups"
+    public static final String KEY_ITEMS = "items"; // JSON key name for the JSON array "items"
+    public static final String KEY_VENUE = "venue"; // JSON key name for the JSON object "venue"
+    public static final String KEY_ID = "id"; // JSON key name for the JSON property  "id"
+    public static final String KEY_NAME = "name"; // JSON key name for the JSON property  "name"
+
+}
