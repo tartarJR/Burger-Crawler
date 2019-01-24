414e9510ed79ee4937515e383435d181c5efd6eb
tartarJR <mustafaogun.ozturk@gmail.com>
2019-01-25 00:56:49 +0300

    Html constants added

---
+++ src/main/java/com/tatar/burgercrawler/constant/HtmlConstants.java
@@ -0,0 +1,11 @@
+package com.tatar.burgercrawler.constant;
+
+public class HtmlConstants {
+
+    public static final String BASE_URL = "https://tr.foursquare.com/v/";
+    public static final String PATH_PHOTOS = "/photos";
+    public static final String TARGET_DIV = "div#wrapper > div#container > div.venueDetail > div.contents > div.mainColumn > div.allPhotosSection > div.photosBlock";
+    public static final String TARGER_PHOTO_CLASS = "mainPhoto";
+    public static final String TARGER_PHOTO_SRC = "src";
+
+}
