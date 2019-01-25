c0d9a078b97b18fd2dfb5be06579511cb2e05b7d
tartarJR <mustafaogun.ozturk@gmail.com>
2019-01-25 16:53:43 +0300

    created venue model

---
+++ src/main/java/com/tatar/burgercrawler/model/Venue.java
@@ -0,0 +1,43 @@
+package com.tatar.burgercrawler.model;
+
+import java.util.List;
+
+public class Venue {
+
+    private String id;
+    private String name;
+    private List<String> photoUrlList;
+
+    public Venue() {
+    }
+
+    public Venue(String id, String name, List<String> photoUrlList) {
+        this.id = id;
+        this.name = name;
+        this.photoUrlList = photoUrlList;
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public List<String> getPhotoUrlList() {
+        return photoUrlList;
+    }
+
+    public void setPhotoUrlList(List<String> photoUrlList) {
+        this.photoUrlList = photoUrlList;
+    }
+}
