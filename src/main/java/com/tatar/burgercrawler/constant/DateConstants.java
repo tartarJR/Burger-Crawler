31ffb80f013f85815bc3bc25e921897c12683138
tartarJR <mustafaogun.ozturk@gmail.com>
2019-01-26 19:59:13 +0300

    added DateConstants class

---
+++ src/main/java/com/tatar/burgercrawler/constant/DateConstants.java
@@ -0,0 +1,15 @@
+package com.tatar.burgercrawler.constant;
+
+public class DateConstants {
+
+    public static final String DATE_FORMAT = "MMMM d, yyyy";
+    public static final long DAY_IN_MS = 1000 * 60 * 60 * 24;
+
+    public static final String SPLIT_REGEX = " ";
+
+    public static final String DAY = "days";
+    public static final String WEEK = "weeks";
+    public static final String MONTH = "months";
+    public static final int DAYS_IN_WEEK = 7;
+
+}
