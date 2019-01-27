84804a979aa0ef632406ec8e1f1e699627013dc9
tartarJR <mustafaogun.ozturk@gmail.com>
2019-01-28 00:05:39 +0300

    Revert "keyzz"
    This reverts commit 53b1646cfca984f6bd5764abc4d371e31aaf688b.

--- src/main/java/com/tatar/burgercrawler/constant/ApiConstants.java
+++ src/main/java/com/tatar/burgercrawler/constant/ApiConstants.java
@@ -6,8 +6,8 @@ public class ApiConstants {
 
     private static final String FS_BASE_URL = "https://api.foursquare.com/v2/venues/explore?";
 
-    private static final String CLIENT_ID = "GRD5JH5OFBGV4RO0N2B24D010CT1WFATH4OSLGGQTL5OLX0Z"; // TODO need a more secure way to store this
-    private static final String CLIENT_SECRET = "HKHWX42VDJW5VMPHYEGQLYJZUMVX2T0D5WEYX24QFS4QHU43"; // TODO need a more secure way to store this
+    private static final String CLIENT_ID = ""; // TODO need a more secure way to store this
+    private static final String CLIENT_SECRET = ""; // TODO need a more secure way to store this
     private static final String VERSION = "20180323";
     private static final String NEAR = "Tartu";
     private static final String CATEGORY_ID = "4d4b7105d754a06374d81259";
