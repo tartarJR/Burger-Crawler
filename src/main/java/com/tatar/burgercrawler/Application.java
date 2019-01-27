dbd88906f3471d91938c95db6a02a2dcbb4ad50d
tartarJR <mustafaogun.ozturk@gmail.com>
2019-01-27 17:39:28 +0300

    refactored application class for async processes

--- src/main/java/com/tatar/burgercrawler/Application.java
+++ src/main/java/com/tatar/burgercrawler/Application.java
@@ -2,9 +2,27 @@ package com.tatar.burgercrawler;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.core.task.TaskExecutor;
+import org.springframework.scheduling.annotation.EnableAsync;
+import org.springframework.scheduling.concurrent.ThreadPoolTaskExecutor;
 
 @SpringBootApplication
+@EnableAsync
 public class Application {
+
+    @Bean(name = "taskExecutor")
+    public TaskExecutor workExecutor() {
+        ThreadPoolTaskExecutor threadPoolTaskExecutor = new ThreadPoolTaskExecutor();
+
+        threadPoolTaskExecutor.setThreadNamePrefix("AsyncHtmlParser-");
+        threadPoolTaskExecutor.setCorePoolSize(3);
+        threadPoolTaskExecutor.setMaxPoolSize(3);
+        threadPoolTaskExecutor.setQueueCapacity(600);
+        threadPoolTaskExecutor.afterPropertiesSet();
+
+        return threadPoolTaskExecutor;
+    }
 
     public static void main(String[] args) {
         SpringApplication.run(Application.class, args);
